cf854b1ee67e0c936ef109c9e65f5663b0015e03
Kamzoki <kaittah@ymail.com>
2017-06-21 21:28:09 +0200

    implemented calling back andriod client

--- Assets/UIFunctions.cs
+++ Assets/UIFunctions.cs
@@ -15,4 +15,35 @@ public class UIFunctions : MonoBehaviour
 		ProjectManager.PM.RT = ProjectManager.RequestType.Pull;
         StartCoroutine(ProjectManager.PM.Request());
 	}
+
+    public void OpenNavClient()
+    {
+        bool fail = false;
+        string bundleId = "";
+        AndroidJavaClass up = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject ca = up.GetStatic<AndroidJavaObject>("currentActivity");
+        AndroidJavaObject packageManager = ca.Call<AndroidJavaObject>("getPackageManager");
+
+        AndroidJavaObject launchIntent = null;
+        try
+        {
+            launchIntent = packageManager.Call<AndroidJavaObject>("getLaunchIntentForPackage", bundleId);
+        }
+        catch (System.Exception e)
+        {
+            fail = true;
+        }
+
+        if (fail)
+        { //open app in store
+            Application.OpenURL("https://google.com");
+        }
+        else //open the app
+            ca.Call("startActivity", launchIntent);
+
+        up.Dispose();
+        ca.Dispose();
+        packageManager.Dispose();
+        launchIntent.Dispose();
+    }
 }
